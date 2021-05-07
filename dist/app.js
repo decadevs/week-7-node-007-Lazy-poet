779b12ac24ef0284a2d2c3c4bb0eef2cdec2180d
Rotimi <70343818+Lazy-poet@users.noreply.github.com>
2021-05-07 04:45:03 +0100

    final fixes

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,7 @@
 "use strict";
-const __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const http_errors_1 = __importDefault(require("http-errors"));
 const express_1 = __importDefault(require("express"));
@@ -25,15 +23,15 @@ app.use("/fetchRecords", fetchRecords_1.default);
 app.use("/calculate", calculate_1.default);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(http_errors_1.default(404));
+    next(http_errors_1.default(404));
 });
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 exports.default = app;
--- dist/routes/fetchRecords.js
+++ dist/routes/fetchRecords.js
@@ -1,9 +1,7 @@
 "use strict";
-const __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
@@ -12,13 +10,14 @@ const router = express_1.default.Router();
 const databasePath = path_1.default.join(__dirname, "../", "database.json");
 let database = [];
 router.get("/", (req, res, next) => {
-  fs_1.default.readFile(databasePath, "utf8", (err, data) => {
-    if (err) {
-      res.status(404).json({ message: "No data found!" });
-    } else {
-      database = JSON.parse(data.toString());
-      res.status(200).json(database);
-    }
-  });
+    fs_1.default.readFile(databasePath, "utf8", (err, data) => {
+        if (err) {
+            res.status(404).json({ message: "No data found!" });
+        }
+        else {
+            database = JSON.parse(data.toString());
+            res.status(200).json(database);
+        }
+    });
 });
 exports.default = router;
